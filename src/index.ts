62ec078d174e7932b89c6dedefd5b429342e9929
Aleksandr Omelnitckii <omelniz@gmail.com>
2023-01-23 16:05:26 +0300

    refactor: remove legacy code

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,1 @@
-import { User } from "./user"
-
-export * from "./channel"
-export * from "./native"
-export * from "./user"
-
-/** Experiences Sdk */
-export const Experiences = {
-	User,
-}
+export default () => {}
