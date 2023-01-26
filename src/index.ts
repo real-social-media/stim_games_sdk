1d4db49f4c72187be179171d9d4fc0e2b477912c
Aleksandr Omelnitckii <omelniz@gmail.com>
2023-01-26 23:20:20 +0300

    feat: export actions

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import IframeEmmiter from "./IframeEmmiter"
 import GamesSDK from "./GamesSDK"
 
+export { IframeActionKind, IframeAction } from "./actions"
+export * as actions from "./actions"
+
 const emmiter = new IframeEmmiter(window.parent)
 
 export default new GamesSDK(emmiter)
